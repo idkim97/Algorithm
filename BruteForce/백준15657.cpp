931c7d2a3e18cba526e3a98db1efc0bf7130d540
idkim97 <idkim9797@naver.com>
2022-01-26 00:21:42 +0900

    Create 백준15657.cpp

---
+++ BruteForce/백준15657.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool c[10];
+int a[10];
+int num[10];
+
+void go(int index, int start, int n, int m) {
+	if (index == m) {
+		for (int i = 0; i < m; i++) {
+			cout << num[a[i]];
+			if (i != m - 1) cout << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = start; i < n; i++) {
+		//if (c[i]) continue;
+		a[index] = i;
+		c[i] = true;
+		go(index + 1,i, n, m);
+		c[i] = false;
+	}
+
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
+
+	go(0, 0,n, m);
+
+	return 0;
+}
