4708ec66bd807af777688d3585f99fb1edb6a1c2
idkim97 <idkim9797@naver.com>
2022-01-27 01:35:58 +0900

    Create 백준15666.cpp

---
+++ BruteForce/백준15666.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool c[10];
+int a[10];
+int num[10];
+vector <vector<int>> d;
+
+void go(int index, int start, int n, int m) {
+	if (index == m) {
+		vector <int> temp;
+		for (int i = 0; i < m; i++) {
+			temp.push_back(num[a[i]]);
+		}
+		d.push_back(temp);
+		return;
+	}
+
+	for (int i = start; i < n; i++) {
+		//if (c[i]) continue;
+		c[i] = true;
+		a[index] = i;
+		go(index + 1, i, n, m);
+		c[i] = false;
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
+	go(0, 0, n, m);
+	// 중복요소 제거
+	sort(d.begin(), d.end());
+	d.erase(unique(d.begin(), d.end()), d.end());
+	for (auto& v : d) {
+		for (int i = 0; i < m; i++) {
+			cout << v[i];
+			if (i != m - 1) {
+				cout << ' ';
+			}
+		}
+		cout << '\n';
+	}
+}
