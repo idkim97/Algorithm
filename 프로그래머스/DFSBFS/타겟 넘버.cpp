394334b458c76390ff958a0d87edd78eae24bd81
idkim97 <idkim9797@naver.com>
2022-05-07 05:11:52 +0900

    Create 타겟 넘버.cpp

---
+++ 프로그래머스/DFSBFS/타겟 넘버.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+
+void dfs(vector<int> numbers, int target, int sum, int cnt) {
+    if (cnt == numbers.size()) {
+        if (sum == target) {
+            answer++;
+        }
+        return;
+    }
+
+    dfs(numbers, target, sum + numbers[cnt], cnt + 1);
+    dfs(numbers, target, sum - numbers[cnt], cnt + 1);
+}
+
+int solution(vector<int> numbers, int target) {
+    answer = 0;
+    dfs(numbers, target, 0, 0);
+
+    return answer;
+}
