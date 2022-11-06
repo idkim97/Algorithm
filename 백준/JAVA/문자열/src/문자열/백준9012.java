6d9db1cf88654d84609a0d1943a4398bda7e903d
idkim97 <idkim9797@naver.com>
2022-11-06 19:51:49 +0900

    Update 백준9012.java

--- 백준/JAVA/문자열/src/문자열/백준9012.java
+++ 백준/JAVA/문자열/src/문자열/백준9012.java
@@ -3,38 +3,30 @@ package 문자열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class 백준9012 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Character> stack = new Stack<>();
 
 		int n = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < n; i++) {
-			String vps = br.readLine();
-			int left = 0;
-			int right = 0;
-			for (int j = 0; j < vps.length(); j++) {
-
-				char temp = vps.charAt(j);
-				if (temp == '(')
-					left++;
-				else if(temp==')')
-					right++;
-
-				if(right>left) {
-					System.out.println("NO");
+			String str = br.readLine();
+			for(int j=0;j<str.length();j++) {
+				char temp = str.charAt(j);
+				if(temp=='(') stack.push('(');
+				else if(!stack.empty() && temp==')') stack.pop();
+				else if(stack.empty() && temp==')') {
+					stack.push(')');
 					break;
 				}
 			}
-			if (left == right) {
-				System.out.println("YES");
-					
-			}
-			else if (left >right){
-				System.out.println("NO");
-				
-			}
+			if(stack.empty()) System.out.println("YES");
+			else System.out.println("NO");
+			stack.clear();
+
 		}
 	}
 }
