695a2d76ce8c5d3abc4ce74efed489f3c0c7f670
idkim97 <idkim9797@naver.com>
2022-11-09 02:13:09 +0900

    Update 백준11721.java

--- 백준/JAVA/문자열/src/문자열/백준11721.java
+++ 백준/JAVA/문자열/src/문자열/백준11721.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 백준11718 {
+public class 백준11721 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
