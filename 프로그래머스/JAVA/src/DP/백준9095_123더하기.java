f5c2a56e30de7a571a97d2ce79e3ff40421b9b83
idkim97 <idkim9797@naver.com>
2022-12-25 20:18:47 +0900

    Create 백준9095_123더하기.java

---
+++ 프로그래머스/JAVA/src/DP/백준9095_123더하기.java
@@ -0,0 +1,26 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준9095_123더하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		int[] d = new int[15];
+		
+		d[0]=1;
+		d[1]=1;
+		d[2]=2;
+		
+		for(int i=0;i<t;i++) {
+			int n = Integer.parseInt(br.readLine());
+			for(int j=3;j<=n;j++) {
+				d[j] =d[j-1]+d[j-2]+d[j-3];				
+			}
+			System.out.println(d[n]);
+		}
+	}
+}
