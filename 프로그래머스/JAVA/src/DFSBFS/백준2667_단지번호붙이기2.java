f866c7e39fdde068c09372fdbc3be5a2ffe3eb61
idkim97 <idkim9797@naver.com>
2023-02-02 00:29:17 +0900

    Update 백준2667_단지번호붙이기2.java

--- 프로그래머스/JAVA/src/DFSBFS/백준2667_단지번호붙이기2.java
+++ 프로그래머스/JAVA/src/DFSBFS/백준2667_단지번호붙이기2.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class 백준2667_단지번호붙이기2 {
@@ -24,6 +26,7 @@ public class 백준2667_단지번호붙이기2 {
 		
 		n = sc.nextInt();
 		map = new int[n][n];
+		visited = new boolean[n][n];
 		
 		for(int i=0;i<n;i++) {
 			String str = sc.next();
@@ -36,35 +39,45 @@ public class 백준2667_단지번호붙이기2 {
 			for(int j=0;j<n;j++) {
 				if(map[i][j]==1 && !visited[i][j]) {
 					apartNum++;
-					dfs(i,j);
+					bfs(i,j);
 				}
 			}
 		}
 		
 		Arrays.sort(aparts);
-		
 		System.out.println(apartNum);
 		
-		for(int i=0;i<aparts.length;i++) {
-			if(aparts[i]!=0) {
-				System.out.println(aparts[i]);				
-			}
-		}
+		for(int i=0; i<aparts.length; i++){
+            if(aparts[i] == 0){
+            }else{
+                System.out.println(aparts[i]);
+            }
+        }
 		
 		
 	}
 	
-	private static void dfs(int x, int y) {
+	private static void bfs(int x, int y) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {x,y});
 		visited[x][y] = true;
 		aparts[apartNum]++;
 		
-		for(int i=0;i<4;i++) {
-			int nx = x+dx[i];
-			int ny = y+dy[i];
+		if(!q.isEmpty()) {
+			int curX = q.peek()[0];
+			int curY = q.peek()[1];
+			q.poll();
 			
-			if(nx>=0 && nx<n && ny>=0 && ny<n) {
-				if(map[nx][ny]==1 && !visited[nx][ny]) {
-					dfs(nx,ny);
+			for(int i=0;i<4;i++) {
+				int nx = curX + dx[i];
+				int ny = curY + dy[i];
+				
+				if(nx>=0 && nx<n && ny>=0 && ny<n) {
+					if(map[nx][ny] ==1 && !visited[nx][ny]) {
+						q.add(new int[] {nx,ny});
+						visited[nx][ny] = true;
+						aparts[apartNum]++;
+					}
 				}
 			}
 		}
