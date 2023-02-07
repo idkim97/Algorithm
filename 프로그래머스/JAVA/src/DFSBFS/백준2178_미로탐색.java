f9b74e30bc8ec81c95e85ffbaf37c1783458690c
idkim97 <idkim9797@naver.com>
2023-02-08 05:24:08 +0900

    Create 백준2178_미로탐색.java

---
+++ 프로그래머스/JAVA/src/DFSBFS/백준2178_미로탐색.java
@@ -0,0 +1,15 @@
+package DFSBFS;
+
+import java.io.IOException;
+
+public class 백준2178_미로탐색 {
+	
+	private static int n;
+	private static int m;
+	private static int[][] map;
+	private static boolean[][] visited;
+	
+	public static void main(String[] args) throws IOException{
+		
+	}
+}
