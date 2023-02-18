a8ec3434832db1d61b2311b4c5412f6fa28457b2
idkim97 <idkim9797@naver.com>
2023-02-18 15:48:45 +0900

    Create 백준10973_이전순열.java

---
+++ 프로그래머스/JAVA/src/BruteForce/백준10973_이전순열.java
@@ -0,0 +1,25 @@
+package BruteForce;
+
+import java.io.*;
+import java.util.*;
+
+public class 백준10973_이전순열 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[10000];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int i = arr.length-1;
+		
+		while(i>=0 && )
+		
+	}
+}
