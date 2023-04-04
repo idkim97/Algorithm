b2653438642ad543dd7049c04590e176793f43df
idkim97 <idkim9797@naver.com>
2023-04-04 15:01:39 +0900

    Create 백준16236_아기상어.java

---
+++ 프로그래머스/JAVA/src/DFSBFS/백준16236_아기상어.java
@@ -0,0 +1,16 @@
+package DFSBFS;
+
+import java.io.*;
+import java.util.*;
+
+public class 백준16236_아기상어 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(br.readLine());
+		
+	}
+}
