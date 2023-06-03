2071273cabb7fcc89dd2d373de4b0db20e45d1d9
Kim Yeoun Su <idkim9797@naver.com>
2023-06-03 17:20:30 +0900

    update

---
+++ 백준/JAVA/algorithm/src/baekjoon/백준10973_이전순열.java
@@ -0,0 +1,57 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class 백준10973_이전순열 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		if(prevPermutation(arr)) {
+			for(int i=0;i<n;i++) {
+				System.out.print(arr[i] + " ");
+			}
+		}else {
+			System.out.println("-1");
+		}
+		
+	}
+	
+	public static boolean prevPermutation(int[] arr) {
+		
+		int a = arr.length-2;
+		
+		while(a>=0 && arr[a]<=arr[a+1]) a--;
+		if(a<0) return false;
+		
+		int b = arr.length-1;
+		while(arr[a]<arr[b]) b--;
+		
+		swap(arr,a,b);
+		
+		int start = a+1;
+		int end = arr.length-1;
+		
+		while(start<end) swap(arr,start++,end--);
+		
+		
+		
+		return true;
+	}
+	
+	public static void swap(int[] arr, int a, int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+}
---
+++ 백준/JAVA/algorithm/src/baekjoon/백준10974_모든순열.java
@@ -0,0 +1,8 @@
+package baekjoon;
+
+public class 백준10974_모든순열 {
+
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
