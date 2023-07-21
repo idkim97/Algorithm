137fa03bb7b8e040e2d034b9eb2b2adcd27de56a
Kim Yeoun Su <74704587+idkim97@users.noreply.github.com>
2023-07-21 13:07:20 +0900

    Create 백준16987_계란으로계란치기.java

---
+++ 백준/JAVA/src/algorithm/백준16987_계란으로계란치기.java
@@ -0,0 +1,41 @@
+package algorithm;
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+
+public class 백준16987_계란으로계란치기 {
+
+    private static int n;
+    private static int[] naegoo;
+    private static int[] weight;
+    private static int res = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        naegoo = new int[n];
+        weight = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            naegoo[i] = a;
+            weight[i] = b;
+        }
+    }
+
+    public static void dfs(int idx) {
+        if (idx == n - 1) {
+
+            return;
+        }
+
+        if (naegoo[idx] > 0) {
+            naegoo[idx]
+
+        }
+    }
+
+}
