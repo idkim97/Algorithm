134399c399d5d04afbac603b1f96b7b6a11ff89a
Kim Yeoun Su <74704587+idkim97@users.noreply.github.com>
2023-08-02 19:56:29 +0900

    Create 백준1915_가장큰정사각형.java

---
+++ 백준/JAVA/src/algorithm/백준1915_가장큰정사각형.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class 백준1915_가장큰정사각형 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+}
